c41ddcefd76e8e8519ee4caec8f46554c9ec4557
Lana <kassembayeva@gmail.com>
2021-06-22 02:42:01 +0000

    the queries 3 are completed

---
+++ 3_queries_group_by/low_average_completion_rime.sql
@@ -0,0 +1,6 @@
+SELECT students.name AS student, ROUND(AVG(assignments.duration)) AS average_estimated_duration, ROUND(AVG(assignment_submissions.duration)) AS average_assignment_duration
+FROM students JOIN assignment_submissions ON students.id = student_id JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
